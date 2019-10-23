9f807cc4aef00c99a5cd8c1e12704b71629521f0
BransonGitomeh <sirbranson@gmail.com>
2019-10-23 13:21:05 +0300

    create complaints based on logged in parent

--- src/graphql/resolvers/Mutation/complaints/index.js
+++ src/graphql/resolvers/Mutation/complaints/index.js
@@ -3,11 +3,11 @@ const { name } = require("./about.js");
 
 const { UserError } = require("graphql-errors");
 
-const create = async (data, { db: { collections } }) => {
+const create = async (data, { db: { collections }, auth }) => {
   const id = new ObjectId().toHexString();
   const entry = Object.assign(data[name], {
     id,
-    parent: "pick parent from jwt",
+    parent: auth.user.id,
     isDeleted: false
   });
 
