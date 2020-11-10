a942d3f7cf23c310ca1402f6a05a08f3756e1457
GitomehBranson <you@example.com>
2020-11-10 10:54:25 +0300

    add school to mpesa confirmation request

--- src/graphql/resolvers/Mutation/payments/index.js
+++ src/graphql/resolvers/Mutation/payments/index.js
@@ -26,7 +26,7 @@ const init = async (data, { db: { collections } }) => {
 
 
 const confirm = async (data, { db: { collections } }) => {
-  const { payment: { CheckoutRequestID, MerchantRequestID } } = data
+  const { payment: { CheckoutRequestID, MerchantRequestID, school } } = data
 
   var options = {
     method: 'POST',
@@ -42,6 +42,38 @@ const confirm = async (data, { db: { collections } }) => {
     const res = await request(options);
 
     const { success, data: { ResultDesc: message } = {} } = res
+
+    // make an entry into the payments collection
+    console.log(res)
+    const meta = JSON.parse(res.data.meta).Item
+
+    meta.map(({Name,Value})=>{
+
+    })
+    // const id = new ObjectId().toHexString();
+    // const entry = Object.assign(data[name], { id, isDeleted: false });
+
+  
+    // school: ,
+    // ammount: ,
+    // type: ,
+    // acc: ,
+    // ref: ,
+    // time: ,
+
+    // {
+    //   success: true,
+    //   data: {
+    //     _id: '5fa97fbecfe35300170e112d',
+    //     id: '5fa97fbe6ebd7a00172ee817',
+    //     MerchantRequestID: '9258-60408806-1',
+    //     CheckoutRequestID: 'ws_CO_09112020204213757172',
+    //     ResultCode: '0',
+    //     ResultDesc: 'The service request is processed successfully.',
+    //     meta: '{"Item":[{"Name":"Amount","Value":2},{"Name":"MpesaReceiptNumber","Value":"OK94CNHIXQ"},{"Name":"TransactionDate","Value":20201109204258},{"Name":"PhoneNumber","Value":254711657108}]}'
+    //   }
+    // }
+
     return {
       success,
       message
